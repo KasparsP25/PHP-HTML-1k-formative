a497117c7f346d1daebb54f33edaf542ebc51986
Emils <emils@gmail.com>
2025-12-05 15:56:29 +0200

    added navigations with <a> between other pages and made it a bit fancy with button

--- contact-sent.php
+++ contact-sent.php
@@ -6,6 +6,7 @@
     <title>Document</title>
 </head>
 <body>
+    
         <?php
     echo("Your info has been saved!" . "<br>");
 echo("Vārds: " . $_POST["fname"] . "<br>" . "e-pasts: " . $_POST["gmail"] . "<br>" . "Jūsu atsauksme: " . $_POST["rev"] . "<br>");
